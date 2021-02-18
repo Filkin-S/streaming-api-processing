934d393ea39a30e9fdc2aadd4b7792c80420d48e
Semion Filkin <fsp1988@yandex.ru>
2021-02-18 11:13:11 +0300

    fixed run_producer.sh

--- run_producer.sh
+++ run_producer.sh
@@ -2,12 +2,12 @@ python -c 'import sys; exit(1) if sys.version_info.major < 3 else exit(0)'
  
 if [[ $? == 0 ]]; then
     [ ! -d "venv" ] && virtualenv -p python venv
-    python -m pip install --upgrade pip
+    pip install --upgrade pip
     source venv/bin/activate
     pip install -r requirements.txt
 else
     [ ! -d "venv" ] && virtualenv -p python3 venv
-    python3 -m pip install --upgrade pip
+    pip install --upgrade pip
     source venv/bin/activate
     pip3 install -r requirements.txt
 fi
