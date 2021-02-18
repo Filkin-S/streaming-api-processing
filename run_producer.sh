a6fe50faa3181a47c446af7aae0947425b93c1b7
Semion Filkin <fsp1988@yandex.ru>
2021-02-18 11:06:05 +0300

    fixed run_producer.sh

--- run_producer.sh
+++ run_producer.sh
@@ -2,12 +2,14 @@ python -c 'import sys; exit(1) if sys.version_info.major < 3 else exit(0)'
  
 if [[ $? == 0 ]]; then
     [ ! -d "venv" ] && virtualenv -p python venv
-    .venv/bin/activate
+    python -m pip install --upgrade pip
+    source venv/bin/activate
     pip install -r requirements.txt
 else
     [ ! -d "venv" ] && virtualenv -p python3 venv
-    .venv/bin/activate
+    python3 -m pip install --upgrade pip
+    source venv/bin/activate
     pip3 install -r requirements.txt
 fi
  
-python ./kafka_producer/producer.py
+python3 ./kafka_producer/producer.py
