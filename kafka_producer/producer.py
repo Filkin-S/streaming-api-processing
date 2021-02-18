47e45feac30c87e8d4665650f9828464136621b7
Semion Filkin <fsp1988@yandex.ru>
2021-02-18 21:58:36 +0300

    deleted excess import from producer

--- kafka_producer/producer.py
+++ kafka_producer/producer.py
@@ -1,6 +1,6 @@
 import requests
 from kafka import KafkaProducer
-from kafka.errors import KafkaError
+
 
 producer = KafkaProducer(bootstrap_servers=['bigdataanalytics-worker-0.novalocal:6667'])
 
@@ -9,4 +9,3 @@ r = requests.get("https://stream.meetup.com/2/rsvps", stream=True)
 for line in r.iter_lines():
 	producer.send('meetup_topic', line)
 
-    
