b2e4e50e7d5929f47a78d1c10061365f7964cba0
Semion Filkin <fsp1988@yandex.ru>
2021-03-01 23:37:51 +0300

    added spark job for inital writing to hdfs kafka archive

---
+++ spark/spark_kafka_to_hdfs_initial.py
@@ -0,0 +1,81 @@
+#export SPARK_KAFKA_VERSION=0.10
+#/spark2.4/bin/pyspark --packages org.apache.spark:spark-sql-kafka-0-10_2.11:2.4.5,com.datastax.spark:spark-cassandra-connector_2.11:2.4.2 --driver-memory 512m --driver-cores 1 --master local[1]
+
+from pyspark.sql import SparkSession
+from pyspark.sql import functions as F
+from pyspark.sql.types import StructType, StructField, StringType, DateType
+
+spark = SparkSession.builder.appName("filkin_spark").getOrCreate()
+
+kafka_brokers = "bigdataanalytics-worker-0.novalocal:6667"
+
+# вычитываем всё, что есть в Кафке
+raw_orders = spark.read. \
+    format("kafka"). \
+    option("kafka.bootstrap.servers", kafka_brokers). \
+    option("subscribe", "meetup_topic"). \
+    option("startingOffsets", "earliest"). \
+    load()
+
+#raw_orders.show(1, False)
+
+# схема для разбора json из value
+schema = StructType([
+    StructField('venue', StructType([
+        StructField('venue_name', StringType(), True),
+        StructField('lon', StringType(), True),
+        StructField('lat', StringType(), True),
+        StructField('venue_id', StringType(), True)
+    ])),
+    StructField('visibility', StringType(), True),
+    StructField('response', StringType(), True),
+    StructField('guests', StringType(), True),
+    StructField('member', StructType([
+        StructField('member_id', StringType(), True),
+        StructField('photo', StringType(), True),
+        StructField('member_name', StringType(), True)
+    ])),
+    StructField('rsvp_id', StringType(), True),
+    StructField('mtime', StringType(), True),
+    StructField('event', StructType([
+        StructField('event_name', StringType(), True),
+        StructField('event_id', StringType(), True),
+        StructField('time', StringType(), True),
+        StructField('event_url', StringType(), True)
+    ])),
+    StructField('group', StructType([
+        StructField('group_topics', StringType(), True),
+        StructField('group_city', StringType(), True),
+        StructField('group_country', StringType(), True),
+        StructField('group_id', StringType(), True),
+        StructField('group_name', StringType(), True),
+        StructField('group_lon', StringType(), True),
+        StructField('group_urlname', StringType(), True),
+        StructField('group_lat', StringType(), True)
+    ])),
+])
+
+# смотрим "чистый" string
+#string_orders = raw_orders \
+#    .select(F.col("value").cast("String").alias("value"))
+
+#string_orders.show(1, False)
+
+# парсим json из string используя schema
+value_orders = raw_orders \
+    .select(F.from_json(F.col("value").cast("String"), schema).alias("value"), "timestamp")
+
+#value_orders.show(1, False)
+
+#value_orders.printSchema()
+
+# раскрываем "nested" структуры в финальный датасет
+parsed_orders = value_orders.select("value.*", "timestamp").select("venue.*", "visibility", "response",
+                                                      "guests", "member.*", "rsvp_id", "mtime",
+                                                      "event.*", "group.*", F.col("timestamp").cast(DateType()).alias("date"))
+
+#parsed_orders.show(1, False)
+
+parsed_orders.write.partitionBy("date").mode(saveMode="overwrite").parquet("meetup_stream_api_files/")
+
+spark.stop()
